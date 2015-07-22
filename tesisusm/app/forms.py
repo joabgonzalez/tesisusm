4031fe81436fb13c2e3e97025744c06647de51c9
Joab Gonzalez <joabgonzalez@gmail.com>
2015-07-22 10:24:38 -0430

    repo anterior

--- tesisusm/app/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from models import *
-
-class UsuarioForm(ModelForm):
-	class Meta:
-		model = Usuario
-		exclude = ("id_user", "id_rol", "fe_create", "status",)
-		widgets = {'clave': forms.PasswordInput()}
